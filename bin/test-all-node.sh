9bade8f31250e84c1b041cd1bc9a92a24a728c61
Natalie Wolfe <natalie@lifewanted.com>
2018-02-07 10:53:37 -0800

    Updated deps and test versions

--- bin/test-all-node.sh
+++ bin/test-all-node.sh
@@ -2,12 +2,12 @@
 
 . "$(brew --prefix nvm)/nvm.sh"
 
-NODE_VERSIONS=(0.10 0.12 4 5 6 7)
+NODE_VERSIONS=(0.12 4 6 8 9)
 
 for version in ${NODE_VERSIONS[@]}; do
   echo " -- Node $version"
   nvm install $version && \
-    npm install --build-from-source && \
+    npm install --no-download && \
     npm test && \
     rm -rf node_modules || \
     exit $?
