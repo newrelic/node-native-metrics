e39160b10af7dd89c0cc86dc3e2bd959566a44cd
Bob Evans <robert.evans25@gmail.com>
2021-07-27 16:18:25 -0400

    disabled all http calls in upload to prevent outgoing calls

--- tests/integration/upload.tap.js
+++ tests/integration/upload.tap.js
@@ -11,22 +11,35 @@ const upload = require('../../lib/upload')
 const nock = require('nock')
 
 tap.test('upload', function(t) {
-  nock('https://nr-downloads-main.s3.amazonaws.com:443')
-    .put(/nodejs_agent/)
-    .reply(200)
-  execSync('rm -rf ./build/Release/*')
-  execSync(`node ./lib/pre-build build native_metrics`)
-  upload('native_metrics', t.end)
+  t.before(() => {
+    nock.disableNetConnect()
+  })
+
+  t.teardown(() => {
+    nock.enableNetConnect()
+  })
+
+  t.test('success', (t) => {
+    nock('https://nr-downloads-main.s3.amazonaws.com:443')
+      .put(/nodejs_agent/)
+      .reply(200)
+    execSync('rm -rf ./build/Release/*')
+    execSync(`node ./lib/pre-build build native_metrics`)
+    upload('native_metrics', t.end)
+  })
+
+  t.test('failure', (t) => {
+    nock('https://nr-downloads-main.s3.amazonaws.com:443')
+      .put(/nodejs_agent/)
+      .reply(404)
+    execSync('rm -rf ./build/Release/*')
+    execSync(`node ./lib/pre-build build native_metrics`)
+    upload('native_metrics', (err) => {
+      t.ok(err, 'should error when not returning 200')
+      t.end()
+    })
+  })
+
+  t.end()
 })
 
-tap.test('upload failure', function(t) {
-  nock('https://nr-downloads-main.s3.amazonaws.com:443')
-    .put(/nodejs_agent/)
-    .reply(404)
-  execSync('rm -rf ./build/Release/*')
-  execSync(`node ./lib/pre-build build native_metrics`)
-  upload('native_metrics', (err) => {
-    t.ok(err, 'should error when not returning 200')
-    t.end()
-  })
-})
