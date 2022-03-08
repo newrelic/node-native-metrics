9257a3679e8e0371670c6472f63eab970a390cc6
Bob Evans <robert.evans25@gmail.com>
2022-03-08 17:00:25 -0500

    added proxy download and fixed the other download test

--- tests/integration/pre-build.tap.js
+++ tests/integration/pre-build.tap.js
@@ -13,6 +13,16 @@ const CMDS = ['build', 'rebuild', 'install']
 const platform = os.platform()
 const NO_PREBUILTS = ['darwin', 'win32']
 
+/**
+ * Download verifies it can retrieve the current version from download.newrelic.com
+ * There are times when `main` has just versioned the new artifact but has not been uploaded
+ * to s3. Also, if running tests locally on mac, these tests are skipped as we only pre build for linux.
+ */
+function skipDownload() {
+  const branch = execSync(`git rev-parse --abbrev-ref HEAD`)
+  return branch === 'main' || NO_PREBUILTS.includes(platform)
+}
+
 function findBinary() {
   return fs.readdirSync('./build/Release').filter((file) => {
     return file.endsWith('.node')
@@ -47,9 +57,7 @@ tap.test('pre-build commands', function (t) {
     t.end()
   })
 
-  // t.test('download', { skip: NO_PREBUILTS.includes(platform) }, function (t) {
-  // this will fail when we release because the new pre-built does not exist
-  t.test('download', { skip: true }, function (t) {
+  t.test('download', { skip: skipDownload() }, function (t) {
     execSync('node ./lib/pre-build --no-build install native_metrics')
     const binary = findBinary()
     t.match(
@@ -58,6 +66,35 @@ tap.test('pre-build commands', function (t) {
       'should download binary'
     )
     t.end()
+  })
+
+  t.test('download with proxy', { skip: skipDownload() }, async function (t) {
+    const { executeCli } = require('../../lib/pre-build')
+    let port
+    const http = require('http')
+    const proxy = require('@newrelic/proxy')
+    const server = proxy(http.createServer())
+    await new Promise((resolve) => {
+      server.listen(() => {
+        port = server.address().port
+        resolve()
+      })
+    })
+    process.env.NR_NATIVE_METRICS_NO_BUILD = 1
+    process.env.NR_NATIVE_METRICS_PROXY_HOST = `http://localhost:${port}`
+    executeCli('install', 'native_metrics')
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        const binary = findBinary()
+        t.match(
+          binary,
+          /_newrelic_native_metrics-\d{1,3}_\d{1,3}_\d{1,3}-native_metrics.*\.node/,
+          'should download binary'
+        )
+        server.close()
+        resolve()
+      }, 2000)
+    })
   })
 
   t.test('invalid cmd(no-op)', function (t) {
