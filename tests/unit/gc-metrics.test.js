d8387d657df315df85ca8f89f52af9bc93980d40
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2024-12-09 15:03:57 -0600

    fix test and unit script

--- tests/unit/gc-metrics.test.js
+++ tests/unit/gc-metrics.test.js
@@ -16,23 +16,23 @@ test('GC Metrics', async (t) => {
   const gcs = metricEmitter.getGCMetrics()
   const keys = Object.keys(gcs)
   assert.ok(keys.length > 0, 'should notice at least one GC')
-  assert.strictEqual(typeof keys[0], 'string', 'should have strings as keys')
+  assert.equal(typeof keys[0], 'string', 'should have strings as keys')
 
   await t.test('GC stats objects', () => {
     const stats = gcs[keys[0]]
-    assert.strictEqual(typeof stats, 'object', 'should have stats objects')
-    assert.strictEqual(typeof stats.typeId, 'number', 'should have the type ID')
-    assert.strictEqual(typeof stats.type, 'string', 'should have the type name')
-    assert.strictEqual(typeof stats.metrics, 'object', 'should have a metrics object')
+    assert.equal(typeof stats, 'object', 'should have stats objects')
+    assert.equal(typeof stats.typeId, 'number', 'should have the type ID')
+    assert.equal(typeof stats.type, 'string', 'should have the type name')
+    assert.equal(typeof stats.metrics, 'object', 'should have a metrics object')
   })
 
   await t.test('GC stats metrics', () => {
     const metrics = gcs[keys[0]].metrics
-    assert.strictEqual(typeof metrics.total, 'number', 'should have total field')
-    assert.strictEqual(typeof metrics.min, 'number', 'should have min field')
-    assert.strictEqual(typeof metrics.max, 'number', 'should have max field')
-    assert.strictEqual(typeof metrics.sumOfSquares, 'number', 'should have sumOfSquares field')
-    assert.strictEqual(typeof metrics.count, 'number', 'should have count field')
+    assert.equal(typeof metrics.total, 'number', 'should have total field')
+    assert.equal(typeof metrics.min, 'number', 'should have min field')
+    assert.equal(typeof metrics.max, 'number', 'should have max field')
+    assert.equal(typeof metrics.sumOfSquares, 'number', 'should have sumOfSquares field')
+    assert.equal(typeof metrics.count, 'number', 'should have count field')
 
     assert.ok(metrics.total > 0, 'should have reasonable values for total')
     assert.ok(metrics.min > 0, 'should have reasonable values for min')
